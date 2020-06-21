de4e62d4464ffcaf4c4e99aab43f8f0879a83c67
jatin2001 <jatinkumarjk2001@gmail.com>
2020-06-21 22:42:58 +0530

    fixing card width

--- src/RobotFriend/Component/Card.js
+++ src/RobotFriend/Component/Card.js
@@ -6,8 +6,8 @@ class Card extends Component
     render()
     {
         return(
-            <div className='bg-light-green dib br3 pa3 ma2 grow shadow-5 tc'>
-                <img src={`https://robohash.org/${this.props.id}?set=set2&size=150x150`} style={{width:'210px'}} alt="photo1"/>
+            <div className='bg-light-green dib br3 pa3 ma2 grow shadow-5 tc' style={{width:'250px'}}>
+                <img src={`https://robohash.org/${this.props.id}?set=set2&size=200x200`} alt="photo1"/>
                 <h3>{this.props.name}</h3>
                 <p>{this.props.email}</p>
             </div>
